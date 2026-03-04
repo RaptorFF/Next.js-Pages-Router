35d5966ac3348fa1590d49ea898e8285a589cfcc
RaptorFF <f.filipovic84@gmail.com>
2026-03-04 16:08:26 +0100

    Routes, Pages and Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello pages router</h1>;
+  return (
+    <>
+      <div>
+        <h1>Hello pages router</h1>
+      </div>
+      <div>
+        <Link href="/about">Go to about page</Link>
+        <Link href="/cabins">Go to cabins page</Link>
+      </div>
+    </>
+  );
 }
